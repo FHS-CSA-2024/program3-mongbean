6f888dc2c19837d7389eec07f142c2c3df78a98d
mongbean <112592782+mongbean@users.noreply.github.com>
2024-08-13 09:17:49 -0700

    finish program3

--- Program3.java
+++ Program3.java
@@ -3,12 +3,27 @@
 import java.util.Scanner; //This imports the console scanner that reads user input
 
 //Your code here:
-
+class Program3{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Length ");
+        int length =  scan.nextInt();
+        System.out.println("Width ");
+        int width =  scan.nextInt();
+        System.out.println("The area is " + width*length + ".");
+        System.out.println("The perimeter is "+2*(width+length)+".");
+    }
+}
 
 
 
 //Paste console output below:
 /*
-
+Length 
+143
+Width 
+83
+The area is 11869.
+The perimeter is 452.
 
 */
